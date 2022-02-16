77afb247e507b62f856b3a06555984f15992cc72
jagdish dev <jagdishdev32@gmail.com>
2022-02-16 11:03:22 +0530

    added locations pages

---
+++ pages/locations.jsx
@@ -0,0 +1,35 @@
+import NavbarGap from "../components/Navbar/NavbarGap";
+import LargeContainerNoFlexColorSection from "../components/Sections/LargeContainerNoFlexColorSection";
+
+const locations = () => {
+  return (
+    <div id="locations">
+      <NavbarGap />
+      <LargeContainerNoFlexColorSection borderEnabled={false}>
+        <h3>Visit one of our many locations today!</h3>
+        <p>
+          Our child care partnerships include Family Child Care and Early Head
+          Start in Montgomery, Clark and Madison counties.
+        </p>
+        <p>
+          Click on the map below to learn more about our more than 50 locations.
+          We have MVCDC-operated early learning and child care programs in
+          Montgomery, Clark, Greene and Madison counties. We also partner with
+          in-home Family Child Care Partners.
+        </p>
+        <p>
+          When using the search function, type in your home or work address and
+          find a center near you.
+        </p>
+
+        <script
+          defer
+          //   src="//jsfiddle.net/0cps2voh/3/embed/result/dark/"
+          src="//jsfiddle.net/0cps2voh/3/embed/result/light/"
+        ></script>
+      </LargeContainerNoFlexColorSection>
+    </div>
+  );
+};
+
+export default locations;
